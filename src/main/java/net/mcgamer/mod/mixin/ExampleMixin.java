833dc3d185a8eb94ec27f4fe20fcda89f3e73dea
ItsTheMCGamer <novapanther1@gmail.com>
2023-06-17 14:00:41 +0200

    Initial Commit

---
+++ src/main/java/net/mcgamer/mod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.mcgamer.mod.mixin;
+
+import net.mcgamer.mod.Mod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Mod.LOGGER.info("");
+	}
+}
